0ecd86de197354acaf63ea9e438918db24c4271c
Paweł Sowa <pawel.sowa@codequest.com>
2023-02-02 20:19:32 +0100

    wip: basic test type

--- src/providers/AppRouterProvider.tsx
+++ src/providers/AppRouterProvider.tsx
@@ -9,6 +9,33 @@ import { LoginPage } from "../pages/LoginPage";
 import { SubroutePage } from "../pages/SubroutePage";
 import { AuthContext } from "./AuthProvider";
 
+const test = [
+  {
+    name: "home" as const,
+    path: "/:id" as const,
+    element: <HomePage />,
+    children: [
+      {
+        name: "nestedRoute" as const,
+        index: true,
+        element: <div>Hello nested route</div>,
+      },
+      {
+        name: "subRoute" as const,
+        path: "subroute/:category",
+        element: <SubroutePage />,
+      },
+    ],
+  },
+  {
+    name: "loginPassThrough" as const,
+    path: "/login" as const,
+    element: <Navigate to="/7bd3a823-e6dd-4ea2-9612-f6defe315cff" />,
+  },
+] as const;
+
+type Test = typeof test[number]["name"];
+
 const authenticatedRouter = createBrowserRouter([
   {
     path: "/:id",
