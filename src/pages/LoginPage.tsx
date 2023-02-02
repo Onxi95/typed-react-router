08384a905e03f5fd18edc64085493d2a9006694b
Paweł Sowa <pawel.sowa@codequest.com>
2023-02-02 19:33:10 +0100

    feat: login button

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,9 +1,17 @@
+import { Button, Box } from "@mui/material";
+import { useContext } from "react";
 import { AppLayout } from "../layouts/AppLayout";
+import { AuthContext } from "../providers/AuthProvider";
 
 export const LoginPage: React.FC = () => {
+  const { setIsAuthenticated } = useContext(AuthContext);
   return (
     <AppLayout>
-      <h1>anonymous page</h1>
+      <Box display="flex" justifyContent="center" marginTop={5}>
+        <Button variant="contained" onClick={() => setIsAuthenticated(true)}>
+          Login
+        </Button>
+      </Box>
     </AppLayout>
   );
 };
