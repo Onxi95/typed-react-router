3be3e89fa9b7bac5757bd665fff2917489c136f7
Paweł Sowa <pawel.sowa@codequest.com>
2023-02-03 11:12:10 +0100

    wip: routing tests

--- src/providers/AppRouterProvider.tsx
+++ src/providers/AppRouterProvider.tsx
@@ -9,10 +9,18 @@ import { LoginPage } from "../pages/LoginPage";
 import { SubroutePage } from "../pages/SubroutePage";
 import { AuthContext } from "./AuthProvider";
 
+type RouteType = {
+  name: string;
+  path?: string;
+  index?: boolean;
+  element: JSX.Element;
+  children?: RouteType[];
+};
+
 const test = [
   {
     name: "home" as const,
-    path: "/:id" as const,
+    path: "/:id",
     element: <HomePage />,
     children: [
       {
@@ -29,12 +37,44 @@ const test = [
   },
   {
     name: "loginPassThrough" as const,
-    path: "/login" as const,
+    path: "/login",
     element: <Navigate to="/7bd3a823-e6dd-4ea2-9612-f6defe315cff" />,
   },
-] as const;
+];
 
-type Test = typeof test[number]["name"];
+type Test1 = typeof test[number] extends { children?: infer ChildrenType }
+  ? ChildrenType extends RouteType[]
+    ? ChildrenType[number]["name"]
+    : never
+  : never;
+type InferNames<T extends ReadonlyArray<RouteType>> = T;
+// type Test2 = InferNames<typeof test>;
+// const test2: ReadonlyArray<RouteType> = [
+//   {
+//     name: "home" as const,
+//     path: "/:id",
+//     element: <HomePage />,
+//     children: [
+//       {
+//         name: "nestedRoute" as const,
+//         index: true,
+//         element: <div>Hello nested route</div>,
+//       },
+//       {
+//         name: "subRoute" as const,
+//         path: "subroute/:category",
+//         element: <SubroutePage />,
+//       },
+//     ],
+//   },
+//   {
+//     name: "loginPassThrough" as const,
+//     path: "/login",
+//     element: <Navigate to="/7bd3a823-e6dd-4ea2-9612-f6defe315cff" />,
+//   },
+// ] as const;
+
+// type Test3 = typeof test;
 
 const authenticatedRouter = createBrowserRouter([
   {
@@ -61,8 +101,13 @@ const anonymousRouter = createBrowserRouter([
     path: "/login",
     element: <LoginPage />,
   },
-  { path: "/", element: <Navigate to="/login" /> },
+  {
+    path: "/",
+    element: <Navigate to="/login" />,
+  },
 ]);
+
+type test = Uncapitalize<"HelloWorld">;
 
 export const AppRouterProvider = () => {
   const { isAuthenticated } = useContext(AuthContext);
