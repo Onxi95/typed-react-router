768142abdbc8e54d6965c9e1a6b497887573d9db
Paweł Sowa <pawel.sowa@codequest.com>
2023-02-05 19:13:18 +0100

    chore: remove unused code

--- src/providers/AppRouterProvider.tsx
+++ src/providers/AppRouterProvider.tsx
@@ -48,33 +48,6 @@ type Test1 = typeof test[number] extends { children?: infer ChildrenType }
     : never
   : never;
 type InferNames<T extends ReadonlyArray<RouteType>> = T;
-// type Test2 = InferNames<typeof test>;
-// const test2: ReadonlyArray<RouteType> = [
-//   {
-//     name: "home" as const,
-//     path: "/:id",
-//     element: <HomePage />,
-//     children: [
-//       {
-//         name: "nestedRoute" as const,
-//         index: true,
-//         element: <div>Hello nested route</div>,
-//       },
-//       {
-//         name: "subRoute" as const,
-//         path: "subroute/:category",
-//         element: <SubroutePage />,
-//       },
-//     ],
-//   },
-//   {
-//     name: "loginPassThrough" as const,
-//     path: "/login",
-//     element: <Navigate to="/7bd3a823-e6dd-4ea2-9612-f6defe315cff" />,
-//   },
-// ] as const;
-
-// type Test3 = typeof test;
 
 const authenticatedRouter = createBrowserRouter([
   {
