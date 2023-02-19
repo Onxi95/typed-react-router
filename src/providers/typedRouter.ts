3cd1cf2f37edcef73cda6a31f9b4f76c38e85b82
Paweł Sowa <pawel@the-sowa.com>
2023-02-19 12:43:37 +0100

    fix: TS recursion errors

--- src/providers/typedRouter.ts
+++ src/providers/typedRouter.ts
@@ -28,7 +28,7 @@ export function createTypedBrowserRouter<T extends ReadonlyArray<RouteType>>(
   const flattenedRoutes = parseNestedRoutes(routerConfig);
   console.log(flattenedRoutes, "flattenedRoutes");
 
-  const buildUrl: BuildUrl<ParsedNestedHash> = (urlName, ...[params]) => {
+  const buildUrl: BuildUrl<ParsedNestedHash> = (...[urlName, ...params]) => {
     console.log(`"${urlName}" params: `, params);
     return flattenedRoutes[urlName];
   };
